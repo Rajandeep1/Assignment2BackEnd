d2bb06596af34254addc6a95a1067a20fab6a5c9
jparmar10 <jparmar190@rrc.ca>
2025-09-25 17:04:58 -0500

    employeeRoute file created

---
+++ src/api/v1/routes/employeeRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router  } from "express";
+import * as employeeController from "../controllers/employeeController";
+
+
+const router: Router = express.Router();
+
+router.get("/", employeeController.getAllEmployees);
+router.post("/", employeeController.createEmployee);
+router.put("/:id", employeeController.updateEmployee);
+router.delete("/:id", employeeController.deleteEmployee);
+
+export default router;
