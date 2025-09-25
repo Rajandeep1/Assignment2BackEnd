78c70f3a047cb244bbf4240a2f81cb6b6831ec7c
jparmar10 <jparmar190@rrc.ca>
2025-09-25 03:21:29 -0500

    branch file created

--- src/data/employees.ts
+++ src/data/employees.ts
@@ -1,9 +1,9 @@
 import { Employees } from "../models/employeeModel";
 
-// exporting the employee data
+// array containing the employee data
 
 export const employees: Employees[] = [
-     {id:1, name: "Alice Johnson", position:"Branch Manager", department:"Management", email:"alice.johnson@pixell-river.com", phone:"604-555-0148", branchId: 1 },
+    {id:1, name: "Alice Johnson", position:"Branch Manager", department:"Management", email:"alice.johnson@pixell-river.com", phone:"604-555-0148", branchId: 1 },
     {id:2 , name: "Amandeep Singh", position:"Customer Service Representative", department:"Customer Service", email:"amandeep.singh@pixell-river.com", phone:"780-555-0172", branchId: 2  },
     {id:3 , name: "Maria Garcia", position:"Loan Officer", department:"Loans", email:"maria.garcia@pixell-river.com", phone:"204-555-0193", branchId:3  },
     {id:4 , name: "James Wilson", position:"IT Support Specialist", department:"IT", email:"james.wilson@pixell-river.com", phone:"604-555-0134", branchId:1},
