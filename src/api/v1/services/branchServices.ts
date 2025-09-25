83d97034ef123feaaba47509643943cbd8fb7c08
jparmar10 <jparmar190@rrc.ca>
2025-09-25 17:30:22 -0500

    createbranch function created in the branchservice.ts file

--- src/api/v1/services/branchServices.ts
+++ src/api/v1/services/branchServices.ts
@@ -9,3 +9,30 @@ import { branches } from "../../../data/branches";
 export const getAllBranches = async(): Promise<Branches[]> => {
     return structuredClone(branches);
 };
+
+/**
+ * id: date.now() will create the unique id
+ * @param newBranchData this function will create the new branch using the given parameters
+ * @returns the newly created branch
+ * 
+ */
+
+export const createBranch = async (branchCreated: {
+    name: string;
+    address: string;
+    phone: string;
+    }
+): Promise<Branches> => {
+    const newBranch: Branches = {
+        id: Date.now(),
+        name: branchCreated.name,
+        address: branchCreated.address,
+        phone: branchCreated.phone,
+    };
+    
+    branches.push(newBranch)
+
+    return structuredClone(newBranch)
+
+
+};
