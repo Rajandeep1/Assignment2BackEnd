01596c0d19a75bf6ef0f8fbd56343589d21c3d2a
jparmar10 <jparmar190@rrc.ca>
2025-09-25 02:49:28 -0500

    employee file created

---
+++ src/models/employeeModel.ts
@@ -0,0 +1,10 @@
+//interface for the employees
+export interface Employees {
+    id: number,
+    name: string,
+    position: string,
+    department: string,
+    email: string,
+    phone: string,
+    branchId: number,
+}
