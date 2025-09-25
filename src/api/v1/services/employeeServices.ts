a2e7358f52d9468d0f490a9212aafacd76020a58
jparmar10 <jparmar190@rrc.ca>
2025-09-25 14:23:22 -0500

    creteemployee function added to the employeeservice.ts file

--- src/api/v1/services/employeeServices.ts
+++ src/api/v1/services/employeeServices.ts
@@ -11,4 +11,36 @@ export const getAllEmployees = async(): Promise<Employees[]> => {
 };
 
 
-e
+/**
+ * 
+ * @param createdEmployee this function will create a new employee using the given parameters
+ * id: date,now() this will create the unique id for employee
+ * @returns return the newly created employye to the end of array
+ */
+
+export const createEmployee = async (createdEmployee: {
+    name: string;
+    position: string;
+    department: string;
+    email: string;
+    phone: string;
+    branchId: number;
+    }
+): Promise<Employees> => {
+    const newEmployee: Employees = {
+        id: Date.now(),
+        name: createdEmployee.name,
+        position: createdEmployee.position,
+        department: createdEmployee.department,
+        email: createdEmployee.email,
+        phone: createdEmployee.phone,
+        branchId: createdEmployee.branchId,
+    };
+
+    // employee.push add the the new adata in the end of the array.
+    employee.push(newEmployee)
+
+    return structuredClone(newEmployee)
+
+
+};
