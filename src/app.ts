3c79a23f8f0b14659ff9b2eca6a9e95c31a9ee3d
Jaspreet Kaur <rajandeepkaur2@rrc.ca>
2025-10-05 19:32:46 -0500

    test for valid employee

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import express, { Express, Request, Response } from "express";
-// Importing morgan
+import express, {Express, Request, Response } from "express";
+// imported morgan 
 import morgan from "morgan";
-
+// this is the import from the employee routes file
 import employeeRoutes from "./api/v1/routes/employeeRoutes"
-
-// Use morgan for HTTP request logging
+// this is the import from the branch routes file
+import branchRoutes from "./api/v1/routes/branchRoutes"
 
 
 const app: Express = express();
+
+// this is the health check interface for response
 
 interface HealthCheckResponse {
     status: string;
@@ -19,27 +21,30 @@ interface HealthCheckResponse {
 
 // Use morgan for HTTP request logging
 app.use(morgan("combined"));
-
+// Ensures that the incoming body is correctly parsed to JSON. otherwise req.body would be undefined
+// post and put body will be empty if i dont write this.
 app.use(express.json());
-
 
 app.get("/", (_req: Request, res: Response ) => {
     res.send("hello world");
 });
 
-// end point for healthdata
+// adding endpoint
 // http://localhost:3000/api/v1/health
 app.get("/api/v1/health", (_req: Request, res: Response) => {
     const healthData: HealthCheckResponse = {
         status: "OK",
         uptime: process.uptime(),
         timestamp: new Date().toISOString(),
-        version: "1.0.0",
+        version: "1.0.0"
     };
 
     res.json(healthData)
 });
 
+// tied routes to main app
 app.use("/api/v1/employees", employeeRoutes)
+app.use("/api/v1/branches", branchRoutes)
+
 
 export default app;
