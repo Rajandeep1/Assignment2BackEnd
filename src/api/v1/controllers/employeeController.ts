b5a7fd4e1580eccde305300046557eaa7044c3d9
Jaspreet Kaur <rajandeepkaur2@rrc.ca>
2025-10-05 19:56:06 -0500

    test for employee update data

--- src/api/v1/controllers/employeeController.ts
+++ src/api/v1/controllers/employeeController.ts
@@ -92,19 +92,50 @@ export const updateEmployee = async (
     next: NextFunction
 ): Promise<void> => {
     try {
+        if (Number.isNaN(req.params.id)) {
+                res.status(HTTP_STATUS.BAD_REQUEST).json({
+                    message:"Invalid ID number"
+                });
+            } else if (!req.body.name) {
+                res.status(HTTP_STATUS.BAD_REQUEST).json({
+                    message:"Employee name is required"
+                });
+            } else if (!req.body.position) {
+                res.status(HTTP_STATUS.BAD_REQUEST).json({
+                    message:"Employee position is required"
+                });
+            } else if (!req.body.department) {
+                res.status(HTTP_STATUS.BAD_REQUEST).json({
+                    message:"Employee department is required"
+                });
+            } else if (!req.body.email) {
+                res.status(HTTP_STATUS.BAD_REQUEST).json({
+                    message:"Employee email is required"
+                });
+            } else if (!req.body.phone) {
+                res.status(HTTP_STATUS.BAD_REQUEST).json({
+                    message:"Employee phone is required"
+                });
+            } else if (req.body.branchId === undefined || req.body.branchId === null) {
+                res.status(HTTP_STATUS.BAD_REQUEST).json({
+                    message: "Employee branchId is required"
+                });
+            } else {
+ 
 
         
-        const id: number = Number(req.params.id);
-        const { name, position, department, email, phone, branchId } = req.body;
-        const updatedEmployee: Employees = await employeeService.updateEmployee(id, { name, position, department, email, phone, branchId });
+                const id: number = Number(req.params.id);
+                const { name, position, department, email, phone, branchId } = req.body;
+                const updatedEmployee: Employees = await employeeService.updateEmployee(id, { name, position, department, email, phone, branchId });
 
-        
-        res.status(HTTP_STATUS.OK).json({
-            message: "Employee data updated successfully",
-            data: updatedEmployee,
-        });
-    } catch (error: unknown) {
-        next(error);
+                
+                res.status(HTTP_STATUS.OK).json({
+                    message: "Employee data updated successfully",
+                    data: updatedEmployee,
+                });
+            }
+         } catch (error: unknown) {
+                next(error);
     };
 };
 
